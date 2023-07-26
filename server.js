bf234fe0d9c7a9f87a08c08a3fbb3b2214a271d9
Manuel Bueno <manuel.bueno@edreamsodigeo.com>
2023-07-26 23:26:37 +0200

    Add call to command line

--- server.js
+++ server.js
@@ -2,29 +2,45 @@
 
 const express = require('express');
 const mqtt = require('mqtt');
+const exec = require('child_process').exec;
 
 // Constants
 const HTTP_PORT = 9001;
 const HTTP_HOST = '0.0.0.0';
 const MQTT_SERVER = '192.168.1.75';
 const MQTT_USER = 'perico2';
-const MQTT_PASSWORD = '053552';
+const MQTT_PASSWORD = '';
 
-// App
-const httpServer = express();
 const mqttClient = mqtt.connect(`mqtt://${MQTT_SERVER}`, {
     username: `${MQTT_USER}`,
     password: `${MQTT_PASSWORD}`
 });
-
-httpServer.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
+mqttClient.on("connect", function () {
+    console.log("connected  " + mqttClient.connected);
+})
+const httpServer = express();
 httpServer.listen(HTTP_PORT, HTTP_HOST, () => {
     console.log(`Running on http://${HTTP_HOST}:${HTTP_PORT}`);
 });
 
+httpServer.get('/', (req, res) => {
+    res.send('Received message');
+    speak("message")
+});
+mqttClient.subscribe('perico2');
 mqttClient.on('message', (topic, message) => {
     console.log(`Received message on topic ${topic}: ${message}`);
+    speak(message)
 });
+
+
+function speak(message) {
+    exec(`echo ${message} | say`,
+        (error, stdout, stderr) => {
+            console.log('stdout:', stdout);
+            if (error !== null) {
+                console.log('exec error: ', error);
+            }
+        }
+    );
+}
