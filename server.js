9dfa4c31061a0460e5193601676298e954f577e6
Manuel Bueno <manuel.bueno@edreamsodigeo.com>
2023-07-26 22:22:15 +0200

    Add MQTT

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 'use strict';
 
 const express = require('express');
+const mqtt = require('mqtt');
 
 // Constants
-const PORT = 9001;
-const HOST = '0.0.0.0';
+const HTTP_PORT = 9001;
+const HTTP_HOST = '0.0.0.0';
+const MQTT_SERVER = '192.168.1.75';
+const MQTT_USER = 'perico2';
+const MQTT_PASSWORD = '053552';
 
 // App
-const app = express();
-app.get('/', (req, res) => {
+const httpServer = express();
+const mqttClient = mqtt.connect(`mqtt://${MQTT_SERVER}`, {
+    username: `${MQTT_USER}`,
+    password: `${MQTT_PASSWORD}`
+});
+
+httpServer.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(PORT, HOST, () => {
-    console.log(`Running on http://${HOST}:${PORT}`);
+httpServer.listen(HTTP_PORT, HTTP_HOST, () => {
+    console.log(`Running on http://${HTTP_HOST}:${HTTP_PORT}`);
+});
+
+mqttClient.on('message', (topic, message) => {
+    console.log(`Received message on topic ${topic}: ${message}`);
 });
