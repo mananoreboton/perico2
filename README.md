516d1c5c5cac8e6ca757a58ed09f825ced48ac47
Manuel Bueno <manuel.bueno@edreamsodigeo.com>
2023-08-22 18:39:06 +0200

    Voice can be selected & Payload as JSON

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-docker build . -t borabora/perico2
+Execution:
+
+- docker build . -t borabora/perico2
+- docker-compose up -d
+
+Local development:
+
+- Download voices in the voices folder (See Dockerfile)
+- Download proper version of Piper (See Dockerfile)
+
+Troubleshooting:
+
+- ALSA lib confmisc.c:165:(snd_config_get_card) Cannot get card index for
+
+1 Set a proper value for plughw:0 in speakCmd
+2 privileged: true i s missing in docker-compose.yml file
--- server.js
+++ server.js
@@ -1,55 +1,84 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const mqtt = require('mqtt');
-const exec = require('child_process').exec;
+const express = require('express')
+const mqtt = require('mqtt')
+const exec = require('child_process').exec
 
 // Constants
-const HTTP_PORT = 9051;
-const HTTP_HOST = '0.0.0.0';
-const MQTT_SERVER = '192.168.1.74';
-const MQTT_USER = 'perico2';
-const MQTT_PASSWORD = '';
+const HTTP_PORT = 9051
+const HTTP_HOST = '0.0.0.0'
+const MQTT_SERVER = '192.168.1.26'
+const MQTT_USER = 'perico2'
+const MQTT_PASSWORD = ''
+const langModels = new Map()
+const langEnModels = new Map()
+const langEsModels = new Map()
+langEnModels.set('f', 'en_US-libritts-high.onnx')
+langEnModels.set('m', 'en_US-ryan-high.onnx')
+langEsModels.set('f', 'es_ES-mls_10246-low.onnx')
+langEsModels.set('m', 'es_ES-sharvard-medium.onnx')
+langModels.set('en_US', langEnModels)
+langModels.set('es_ES', langEsModels)
 
 const mqttClient = mqtt.connect(`mqtt://${MQTT_SERVER}`, {
     username: `${MQTT_USER}`,
     password: `${MQTT_PASSWORD}`
-});
+})
 mqttClient.on("connect", function () {
-    console.log("MQTT connected  " + mqttClient.connected);
+    console.log("MQTT connected  " + mqttClient.connected)
 })
 mqttClient.on('error', function(err) {
-    console.log(err);
-});
-const httpServer = express();
-httpServer.listen(HTTP_PORT, HTTP_HOST, () => {
-    console.log(`Running on http://${HTTP_HOST}:${HTTP_PORT}`);
-});
+    console.log(err)
+})
+mqttClient.subscribe('perico2')
 
-httpServer.get('/', (req, res) => {
-    res.send('Received message');
-    speak("message")
-});
-mqttClient.subscribe('perico2');
 mqttClient.on('message', (topic, message) => {
-    console.log(`Received message on topic ${topic}: ${message}`);
-    speak(message)
-});
+    //console.log(`Received message on topic ${topic}: ${message}`)
+    procesPayload(message)
+})
 
+const httpServer = express()
+httpServer.listen(HTTP_PORT, HTTP_HOST, () => {
+    console.log(`Running on http://${HTTP_HOST}:${HTTP_PORT}`)
+})
 
-function speak(message) {
-    console.log(`Speaking: ${message}`)
-    if (message.length > 0) {
-        exec(`echo ${message} | ./piper/piper --model langs/en_US-libritts-high.onnx --output_raw | aplay --channels=1 --file-type raw --rate=22050 -f S16_LE -D plughw:1`,
-            (error, stdout, stderr) => {
-                console.log('stdout:', stdout);
-                if (error !== null) {
-                    console.log('exec error: ', error);
-		    if (error.signal !== null) {
-	                console.log('signal: ', error.signal);
-		    }
+httpServer.post('/', (req, res) => {
+    res.send('Received message')
+    procesPayload(req.body)
+})
+
+function procesPayload(message) {
+    const json = JSON.parse(message)
+    if (json.payload.action === 'speak') {
+        speak(json.payload.lang, json.payload.voice, json.payload.text);
+    }
+}
+
+function speak(lang, voice, text) {
+
+    if (text.length > 0) {
+
+        const modelLang = langModels.get(lang)
+        if (modelLang) {
+            const modelVoice = modelLang.get(voice)
+            if (modelVoice) {
+                if (voice) {
+                    console.log(`Speaking [${lang}][${voice}]: ${text}`)
+                    const speakCmd = `echo '${text}' | ./piper/piper --model voices/${modelVoice} --output_raw | aplay --channels=1 --file-type raw --rate=22050 -f S16_LE -D plughw:1`
+                    console.log(speakCmd)
+                    exec(speakCmd,
+                        (error, stdout, stderr) => {
+                            console.log('stdout:', stdout)
+                            if (error !== null) {
+                                console.log('exec error: ', error)
+                                if (error.signal !== null) {
+                                    console.log('signal: ', error.signal)
+                                }
+                            }
+                        }
+                    )
                 }
             }
-        );
+        }
     }
 }
